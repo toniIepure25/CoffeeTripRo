e35d2e9098147bc5cce516a99a0badf2b44a53ee
Iepure Antoniu <iepuretoni533@gmail.com>
2022-11-27 19:13:11 +0200

    another commit

--- app.js
+++ app.js
@@ -138,6 +138,10 @@ app.get("/products/:postName", function(req, res){
   })
 })
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function () {
+//   console.log("Server started on port 3000");
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("SERVER STARTED PORT: 3000");
+})

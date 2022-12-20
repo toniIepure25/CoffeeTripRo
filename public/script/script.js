e7e96d6a45ecb4db1785d49f21c4d889f05e8c08
Iepure Antoniu <iepuretoni533@gmail.com>
2022-12-20 12:59:07 +0200

    cookies

--- public/script/script.js
+++ public/script/script.js
@@ -38,47 +38,21 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 
-// const autoSlide = () => {
-//   if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth)
-//     return;
+let cookieModal = document.querySelector('.cookie-consent-modal');
+let cancelCookieBtn = document.querySelector('.cbtn.cancel');
+let acceptCookieBtn = document.querySelector('.cbtn.accept');
 
-//   positionDiff = Math.abs(positionDiff);
-//   let firstImgWidth = firstImg.clientWidth + 18;
-//   let valDifference = firstImgWidth - positionDiff;
+cancelCookieBtn.addEventListener('click', function() {
+  cookieModal.classList.remove('active');
+});
+acceptCookieBtn.addEventListener('click', function() {
+  cookieModal.classList.remove('active');
+  localStorage.setItem('cookieAccepted', "yes");
+});
 
-//   if (carousel.scrollLeft > prevScrollLeft) {
-//     return (carousel.scrollLeft +=
-//       positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
-//   }
-//   carousel.scrollLeft -=
-//     positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-// };
-
-// const dragStart = (e) => {
-//   isDragStart = true;
-//   prevPageX = e.pageX || e.touches[0].pageX;
-//   prevScrollLeft = carousel.scrollLeft;
-// };
-
-// const dragging = (e) => {
-//   if (!isDragStart) return;
-//   e.preventDefault();
-//   carousel.classList.add("dragging");
-//   positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-//   carousel.scrollLeft = prevScrollLeft - positionDiff;
-// };
-
-// const dragStop = () => {
-//   isDragStart = false;
-//   carousel.classList.remove("dragging");
-//   autoSlide();
-// };
-// carousel.addEventListener("mousedown", dragStart);
-// carousel.addEventListener("touchstart", dragStart);
-
-// carousel.addEventListener("mousemove", dragging);
-// carousel.addEventListener("touchmove", dragging);
-
-// carousel.addEventListener("mouseup", dragStop);
-// carousel.addEventListener("mouseleave", dragStop);
-// carousel.addEventListener("touchend", dragStop);
+setTimeout(() => {
+  let cookieAccepted = localStorage.getItem('cookieAccepted');
+  if(cookieAccepted != 'yes') {
+    cookieModal.classList.add('active');
+  }
+}, 2000);
